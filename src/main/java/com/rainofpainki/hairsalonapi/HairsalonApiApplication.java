50629bdc267c79c1bd5aa6e562692deebe501b01
Sunha Ryu <rainofpainki@naver.com>
2023-04-25 10:41:10 +0900

    feat: Project Setup
    Spring Boot 설치

---
+++ src/main/java/com/rainofpainki/hairsalonapi/HairsalonApiApplication.java
@@ -0,0 +1,13 @@
+package com.rainofpainki.hairsalonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairsalonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairsalonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rainofpainki/hairsalonapi/HairsalonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainofpainki.hairsalonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairsalonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
