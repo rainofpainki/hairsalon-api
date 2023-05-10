523a466e62dd823a4babfc79b7e305a9ad1043ec
Sunha Ryu <rainofpainki@naver.com>
2023-05-10 12:12:31 +0900

    chore(*): 데이터베이스 초기값 설정 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,12 +2,12 @@ Insert into SHOP (SHOP_ID,SHOP_NAME,SHOP_THUMB_URL,SHOP_ADDRESS,SHOP_START_HOUR_
 최고의 실력과 센스를 겸비한 디자이너에게 프라이빗 한 공간에서 스타일링을 받아보세요:)
 또한, 저렴한 약제는 취급하지 않으며 고퀄리티의 약제만 사용하는 것을 약속 드립니다.
 
-100% 예약제로 운영되고 있습니다 :)',1);
+100% 예약제로 운영되고 있습니다 :)',1)/
 Insert into SHOP (SHOP_ID,SHOP_NAME,SHOP_THUMB_URL,SHOP_ADDRESS,SHOP_START_HOUR_OF_WEEKDAY,SHOP_END_HOUR_OF_WEEKDAY,SHOP_START_HOUR_OF_WEEKEND,SHOP_END_HOUR_OF_WEEKEND,SHOP_TEL_NUMBER,SHOP_MESSAGE,SHOP_HOLIDAY_OF_WEEK) values (2,'아데르 역삼점','/images/shop2.jpg','서울 강남구 테헤란로 142 아크플레이스빌딩 B1층','10:00','21:30','10:00','20:00','0507-1349-1172','프라이빗룸 1인 미용실, 디자이너 책임제
 
 - -
 처음부터 끝까지 모든 시술 과정이 1인 디자이너 책임제로 운영됩니다.
-모든 시술 공간이 1:1 프라이빗 룸으로 갖추어진 최대 규모의 토탈 뷰티 살롱에서 최상의 뷰티케어를 받으세요.',null);
+모든 시술 공간이 1:1 프라이빗 룸으로 갖추어진 최대 규모의 토탈 뷰티 살롱에서 최상의 뷰티케어를 받으세요.',null)/
 Insert into SHOP (SHOP_ID,SHOP_NAME,SHOP_THUMB_URL,SHOP_ADDRESS,SHOP_START_HOUR_OF_WEEKDAY,SHOP_END_HOUR_OF_WEEKDAY,SHOP_START_HOUR_OF_WEEKEND,SHOP_END_HOUR_OF_WEEKEND,SHOP_TEL_NUMBER,SHOP_MESSAGE,SHOP_HOLIDAY_OF_WEEK) values (3,'지노헤어 성균관대점','/images/shop3.jpg','서울 종로구 성균관로 21-2','11:00','21:00','11:00','21:00','0507-1371-2269','안녕하세요 에이바헤어 종각역점 입니다
 고객님과의 소중한 약속을 지킬수 있게 노력하겠습니다
 
@@ -15,4 +15,4 @@ Insert into SHOP (SHOP_ID,SHOP_NAME,SHOP_THUMB_URL,SHOP_ADDRESS,SHOP_START_HOUR_
 
 2.더욱 더 좋은 기술력으로 더 많이 배우고 더 많은 노력으로 보답하겠습니다
 
-3.시술 전 상담에 최선을 다하며 친절한 매장이 되겠습니다',2);
+3.시술 전 상담에 최선을 다하며 친절한 매장이 되겠습니다',2)/
